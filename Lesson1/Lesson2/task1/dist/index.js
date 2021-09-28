f61134873ae265ec5c5d60d6a448a920b7da1b38
svlanakom <svetakom.222@gmail.com>
2021-09-28 13:29:09 +0300

    lesson2task1

---
+++ Lesson1/Lesson2/task1/dist/index.js
@@ -0,0 +1,9 @@
+import { printProfile } from "./profile.js";
+const userData = {
+  name: "Tom",
+  age: 17
+};
+const profile = { ...userData,
+  company: "Gromcode"
+};
+printProfile(profile);
---
+++ Lesson1/Lesson2/task1/dist/profile.js
@@ -0,0 +1,7 @@
+const printProfile = profileData => {
+  const {
+    name,
+    company
+  } = profileData;
+  console.log(`${name} form ${company}`);
+};

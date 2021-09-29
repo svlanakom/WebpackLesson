313ace4e164cdce65ba7041c92d0974c3229742a
svlanakom <svetakom.222@gmail.com>
2021-09-29 12:04:41 +0300

    Lesson5task2

---
+++ Lesson1/Lesson5/task3/dist/profile.js
@@ -0,0 +1,13 @@
+(() => {
+  "use strict";
+  (async function (t) {
+    const o = await fetch("https://api.github.com/users/github");
+    if (!o.ok) throw new Error("Failed to get user data");
+    return await o.json();
+  })().then(({ name: t, location: o }) =>
+    ((t) => {
+      const { name: o, location: n } = t;
+      console.log(`${o} from ${n}`);
+    })({ name: t, location: o })
+  );
+})();
